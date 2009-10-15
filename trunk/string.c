033cc0777a79b1d6fceeb280a6d4e6ae2648b14d
mvm9289 <mvm9289@30dffabe-b91f-11de-88a7-9da883da0866>
2009-10-15 00:19:22 +0000

    ZeOS 1.1 Final

---
+++ trunk/string.c
@@ -0,0 +1,43 @@
+/*
+ * string.c - 
+ */
+
+#include <string.h>
+
+int strlen(char *buffer)
+{
+
+    int i = 0;
+    while(buffer[i] != '\0') ++i;
+    return i;
+
+}
+
+void itoa(int num, char *buffer)
+{
+
+    int i, j, sign;
+    char aux;
+
+    buffer[0]='\0';
+
+    if((sign = num) < 0) num=-num;
+
+    i=1;
+    j=0;
+    do {
+        buffer[i++]=num%10 + '0';
+        j++;
+    } while ((num /= 10) > 0);
+
+    if(sign < 0) buffer[i]='-';
+
+
+    for (i = 0; i < j; i++, j--)
+    {
+        aux = buffer[i];
+        buffer[i] = buffer[j];
+        buffer[j] = aux;
+    }
+
+}
