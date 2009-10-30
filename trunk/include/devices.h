343a3689b765382566283d3bd5319141a17c7217
mvm9289 <mvm9289@30dffabe-b91f-11de-88a7-9da883da0866>
2009-10-30 10:51:56 +0000

    Structs de la primera parte de la tercera entrega

--- trunk/include/devices.h
+++ trunk/include/devices.h
@@ -1,5 +1,34 @@
 #ifndef DEVICES_H__
 #define  DEVICES_H__
 
+#define FILE_NAME_SIZE 10
+#define DIR_ENTRYS 10
+#define CTABLE_SIZE 10
+
+#define O_RDONLY 0
+#define O_WRONLY 1
+#define O_RWONLY 2
+
+struct file_operations {
+    void *read;
+    void *write;
+};
+
+struct logic_device {
+    char name[FILE_NAME_SIZE];
+    int acces_mode;
+    file_operations *operations;
+};
+
+struct directory {
+    logic_device dir_entrys[DIR_ENTRYS];
+};
+
+struct chanel {
+    int seq_pos;
+    int init_acces_mode;
+};
+
 int sys_write_console(char *buffer,int size);
+int sys_read_keyboard();
 #endif /* DEVICES_H__*/
