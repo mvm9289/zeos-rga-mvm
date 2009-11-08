8fab063bf9c8bb64af9141283cd865c164c94dca
mvm9289 <mvm9289@30dffabe-b91f-11de-88a7-9da883da0866>
2009-11-08 19:26:13 +0000

    4 chorradas del keyboard

--- trunk/include/keyboard.h
+++ trunk/include/keyboard.h
@@ -1,4 +1,11 @@
 #ifndef __KEYBOARD_H__
 #define __KEYBOARD_H__
 
+#define KEYBUFF_SIZE 128
+
+extern struct list_head keyboardqueue;
+extern char keyboard_buffer[KEYBUFF_SIZE];
+int ini_pos;
+int end_pos;
+
 #endif /* __KEYBOARD_H__ */
