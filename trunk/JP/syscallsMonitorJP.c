642a2bb36d198379d2123f8807ae3b6df69b9e5f
mvm9289 <mvm9289@30dffabe-b91f-11de-88a7-9da883da0866>
2009-12-14 21:32:56 +0000

    Añado un juego de pruebas que he hecho, no acabo de saber como hacer k el clone falle, lo demas va todo bien aunk creo k al final he dejado el fichero con mierdas puestas pk estaba probrando.

---
+++ trunk/JP/syscallsMonitorJP.c
@@ -0,0 +1,221 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/errno.h>
+
+#define PRINTFS 42
+
+void suicide_me() {
+    exit(0);
+}
+
+int main() {
+    char buffer[256];
+
+    int good_opens = 0;
+    int bad_opens = 0;
+    int good_closes = 0;
+    int bad_closes = 0;
+    int good_writes = 0;
+    int bad_writes = 0;
+    int good_lseeks = 0;
+    int bad_lseeks = 0;
+    int good_clones = 0;
+    int bad_clones = 0;
+
+    int fd0;
+    int fd1;
+    int fd2;
+
+    int i;
+    unsigned int bad_result = 0;
+
+    printf("\n\nTESTS FOR SYSCALLS MONITOR MODULE\n\n");
+    printf("My PID is %d.\n\n", getpid());
+    printf("Press ENTER to start tests.");
+    fflush(stdout);
+    read(0, NULL, 1);
+    printf("\nTests Runing...\n");
+
+    printf("\nTEST 1: Open a non exist file with result OK.");
+    fd1 = open("newfile", O_RDWR | O_CREAT | O_EXCL, 0777);
+    if (fd1 == -1) {
+        printf("----->Result BAD\n");
+        perror(buffer);
+        printf(buffer);
+        good_writes++;
+        bad_opens++;
+    }
+    else {
+        printf("----->Result OK\n");
+        good_opens++;
+    }
+    
+    printf("\nTEST 2: Open an already exist file with result OK.");
+    fd1 = open("newfile", O_RDWR, 0777);
+    fd0 = fd1;
+    if (fd1 == -1) {
+        printf("----->Result BAD\n");
+        perror(buffer);
+        printf(buffer);
+        good_writes++;
+    }
+    else {
+        printf("----->Result OK\n");
+        good_opens++;
+    }
+
+    printf("\nTEST 3: Open an already exist file with result BAD.");
+    fd1 = open("newfile", O_RDWR | O_CREAT | O_EXCL, 0777);
+    if (fd1 == -1) {
+        printf("----->Result OK\n");
+        bad_opens++;
+    }
+    else {
+        printf("----->Result BAD\n");
+        good_opens++;
+    }
+
+    printf("\nTEST 4: Open a non exist file with result BAD.");
+    fd1 = open("newfile2", O_RDWR, 0777);
+    if (fd1 == -1) {
+        printf("----->Result OK\n");
+        bad_opens++;
+    }
+    else {
+        printf("----->Result BAD\n");
+        good_opens++;
+    }
+
+    printf("\nTEST 5: Close an exist fd with result OK. (x5)");
+    for (i = 0; i < 5; i++) {
+        fd1 = dup(2);
+        fd2 = close(fd1);
+        if (fd2 == -1) {
+            perror(buffer);
+            printf(buffer);
+            bad_closes++;
+            good_writes += 5;
+            bad_result = 1;
+        }
+        else good_closes++;
+    }
+    if (bad_result) printf("----->Result BAD\n");
+    else printf("----->Result OK\n");
+    bad_result = 0;
+
+    printf("\nTEST 6: Close a non exist fd with result BAD.(x5)");
+    fd1 = -1;
+    for (i = 0; i < 5; i++) {
+        fd2 = close(fd1);
+        if (fd2 == -1) {
+            bad_closes++;
+            bad_result = 1;
+        }
+        else good_closes++;
+    }
+    if (bad_result) printf("----->Result OK\n");
+    else printf("----->Result BAD\n");
+    bad_result = 0;
+
+    printf("\nTEST 7: Write a non exist fd with result BAD.(x5)");
+    for (i = 0; i < 5; i++) {
+        fd1 = write(-1, "\nTHIS SHOULD NOT SHOW\n", 20);
+        if (fd1 == -1) {
+            bad_writes++;
+            bad_result = 1;
+        }
+        else good_writes++;
+    }
+    if (bad_result) printf("----->Result OK\n");
+    else printf("----->Result BAD\n");
+    bad_result = 0;
+
+    printf("\nTEST 8: Lseek an exist fd with result OK.(x5)");
+    for (i = 0; i < 5; i++) {
+        fd1 = lseek(fd0, 0, SEEK_END);
+        if (fd1 == -1) {
+            perror(buffer);
+            printf(buffer);
+            bad_lseeks++;
+            good_writes += 5;
+            bad_result = 1;
+        }
+        else good_lseeks++;
+    }
+    if (bad_result) printf("----->Result BAD\n");
+    else printf("----->Result OK\n");
+    bad_result = 0;
+
+    printf("\nTEST 9: Lseek a non exist fd with result BAD.(x5)");
+    for (i = 0; i < 5; i++) {
+        fd1 = lseek(-1, 0, SEEK_END);
+        if (fd1 == -1) {
+            bad_lseeks++;
+            bad_result = 1;
+        }
+        else good_lseeks++;
+    }
+    if (bad_result) printf("----->Result OK\n");
+    else printf("----->Result BAD\n");
+    bad_result = 0;
+
+    printf("\nTEST 10: Clone with result OK.(x5)");
+    for (i = 0; i < 5; i++) {
+        //fd1 = clone((void *)exit, buffer, CLONE_SIGHAND, NULL);
+        if (fd1 == -1) {
+            perror(buffer);
+            printf(buffer);
+            bad_clones++;
+            good_writes += 5;
+            bad_result = 1;
+        }
+        else good_clones++;
+    }
+    if (bad_result) printf("----->Result BAD\n");
+    else printf("----->Result OK\n");
+    bad_result = 0;
+
+    /*printf("\nTEST 11: Clone with result BAD.(x5)");
+    for (i = 0; i < 5; i++) {
+        fd1 = clone((void *)exit, buffer, -1, NULL);
+        if (fd1 == -1) {
+            bad_clones++;
+            bad_result = 1;
+        }
+        else good_clones++;
+    }
+    if (bad_result) printf("----->Result OK\n");
+    else printf("----->Result BAD\n");
+    bad_result = 0;*/
+
+    unlink("newfile");
+
+    printf("\n\nEXPECTED RESULTS OF MODULE:\n");
+    printf("\nOPEN:\n");
+    printf("Total calls: %d\n", good_opens+bad_opens);
+    printf("Good calls: %d\n", good_opens);
+    printf("Bad calls: %d\n", bad_opens);
+    printf("\nCLOSE:\n");
+    printf("Total calls: %d\n", good_closes+bad_closes);
+    printf("Good calls: %d\n", good_closes);
+    printf("Bad calls: %d\n", bad_closes);
+    printf("\nWRITE:\n");
+    printf("Total calls: %d\n", good_writes+bad_writes+PRINTFS);
+    printf("Good calls: %d\n", good_writes+PRINTFS);
+    printf("Bad calls: %d\n", bad_writes);
+    printf("\nLSEEK:\n");
+    printf("Total calls: %d\n", good_lseeks+bad_lseeks);
+    printf("Good calls: %d\n", good_lseeks);
+    printf("Bad calls: %d\n", bad_lseeks);
+    printf("\nCLONE:\n");
+    printf("Total calls: %d\n", good_clones+bad_clones);
+    printf("Good calls: %d\n", good_clones);
+    printf("Bad calls: %d\n", bad_clones);
+
+    while(1);
+}
